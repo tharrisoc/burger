87f011ae15980042852aba9b2d24d14d18fbc45f
Thomas Harris <tomwharris@twharris.onmicrosoft.com>
2018-05-06 09:58:04 -0700

    Modified connection.js so that it can connect to an Heroku database in addition to a local database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "tom",
+    password: "J2MeDev",
+    database: "burgers_db"
+  });
+}
+
+connection = mysql.createConnection({
   host: "localhost",
   user: "tom",
   password: "J2MeDev",
