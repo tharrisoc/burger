983e50382aaeaf14081e2fd521d14be7e0922271
Thomas Harris <tomwharris@twharris.onmicrosoft.com>
2018-05-06 21:00:59 -0700

    Removed unconditional second call to mysql.createConnection() for non-Heroku case

--- config/connection.js
+++ config/connection.js
@@ -13,13 +13,6 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-connection = mysql.createConnection({
-  host: "localhost",
-  user: "tom",
-  password: "J2MeDev",
-  database: "burgers_db"
-});
-
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
